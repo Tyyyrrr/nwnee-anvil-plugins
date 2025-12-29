fc5748d7e559d5c63e7e5eceb189dffd972140c1
Tyyyrrr <michalkuglin@gmail.com>
2025-12-29 20:44:33 +0100

    rename project and main window

--- QuestSystem/Editor/EditorWindow.xaml.cs
+++ QuestSystem/Editor/EditorWindow.xaml.cs
@@ -14,9 +14,9 @@ namespace Editor;
 /// <summary>
 /// Interaction logic for MainWindow.xaml
 /// </summary>
-public partial class MainWindow : Window
+public partial class EditorWindow : Window
 {
-    public MainWindow()
+    public EditorWindow()
     {
         InitializeComponent();
     }
