32f7b52fea2d5ab21d5472e06df6751cf22ccfbf
Tyrr <michalkuglin@gmail.com>
2026-01-22 10:36:04 +0100

    feat! (ServerData): add new subrace checks

--- ServerData/ICreatureInspector.cs
+++ ServerData/ICreatureInspector.cs
@@ -18,6 +18,17 @@ namespace ServerData
         public bool IsSatyr(NwCreature creature);
         public bool IsNymph(NwCreature creature);
         public bool IsWaterNymph(NwCreature creature);
+        public bool IsSkulldwarf(NwCreature creature);
+        public bool IsCitydwarf(NwCreature creature);
+        public bool IsDesertElf(NwCreature creature);
+        public bool IsUnderdweller(NwCreature creature);
+        public bool IsSvart(NwCreature creature);
+        public bool IsWolffolk(NwCreature creature);
+        public bool IsCossack(NwCreature creature);
+        public bool IsTatar(NwCreature creature);
+        public bool IsGypsy(NwCreature creature);
+        public bool IsAsian(NwCreature creature);
+        public bool IsArabian(NwCreature creature);
 
         // Body state
         public bool IsFlying(NwCreature creature);
