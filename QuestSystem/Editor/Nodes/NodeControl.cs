2d4aa5107af475552426dc75231b995d7043daf8
Tyrr <michalkuglin@gmail.com>
2026-01-24 22:30:52 +0100

    basic custom controls for nodes

---
+++ QuestSystem/Editor/Nodes/NodeControl.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace QuestEditor.Nodes
+{
+    public class NodeControl : Control
+    {
+        public static readonly DependencyProperty ContentProperty = DependencyProperty.Register(
+            nameof(Content), 
+            typeof(object),
+            typeof(NodeControl), 
+            new PropertyMetadata(null)); 
+
+        public object Content 
+        { 
+            get => GetValue(ContentProperty); 
+            set => SetValue(ContentProperty, value); 
+        }
+
+        static NodeControl()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(NodeControl), new FrameworkPropertyMetadata(typeof(NodeControl)));
+        }
+    }
+}
