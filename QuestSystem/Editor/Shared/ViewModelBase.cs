559d77b800ff731dc5d3b1f23820f5698c6e68f3
Tyrr <michalkuglin@gmail.com>
2026-01-24 22:30:52 +0100

    basic custom controls for nodes

---
+++ QuestSystem/Editor/Shared/ViewModelBase.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace QuestEditor
+{
+    public abstract class ViewModelBase : INotifyPropertyChanging, INotifyPropertyChanged
+    {
+        public event PropertyChangingEventHandler? PropertyChanging;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if(Equals(storage,value)) 
+                return false;
+
+            PropertyChanging?.Invoke(this, new PropertyChangingEventArgs(propertyName));
+            storage = value;
+            RaisePropertyChanged(propertyName ?? string.Empty);
+
+            return true;
+        }
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
